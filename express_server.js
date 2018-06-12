0af223d9451712dd1cdc6e136bbd65779aad9190
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-06-12 23:53:40 +0000

    added body-parser and new page with submission form for long urls

--- express_server.js
+++ express_server.js
@@ -2,17 +2,21 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "5jEF5z": "http://www.lovemeow.com"
 };
 
 app.get("/", (request, response) => {
   response.end("Hello World!");
 });
-
+//////Exercise examples/////////////////
 //displaying urlDatabase as json object on the website
 app.get("/urls.json", (request, response) => {
   response.json(urlDatabase);
@@ -23,9 +27,15 @@ app.get("/hello", (request, response) => {
   response.end("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+/////////////////////////////////////////
+
 app.get("/urls", (request, response) => {
   let templateVars = { urls: urlDatabase };
   response.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (request, response) => {
+  response.render("urls_new");
 });
 
 app.get("/urls/:id", (request, response) => {
@@ -33,7 +43,22 @@ app.get("/urls/:id", (request, response) => {
   response.render("urls_show", templateVars);
 });
 
+app.post("/urls", (request, response) => {
+  console.log(request.body);  // debug statement to see POST parameters
+  response.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
 ///////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let randomStr = "";
+  const items = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++)
+    randomStr += items.charAt(Math.floor(Math.random() * items.length));
+  return randomStr;
+}
+
+//(generateRandomString());
