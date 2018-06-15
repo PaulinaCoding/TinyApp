94bcb86113d32864fbb085880ed28789d028bbdd
PaulinaCoding <paulina.pietrykiewicz@gmail.com>
2018-06-15 00:15:30 +0000

    Moved login from heather to seperate page, updated cookies added links to heather

--- express_server.js
+++ express_server.js
@@ -13,20 +13,31 @@ app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": {
+    url: "http://www.lighthouselabs.ca",
+    userID: "RandomId1"
+   },
+  "9sm5xK": {
+    url: "http://www.google.com",
+    userID: "RandomId2"
+  }
 };
 
 const users = { 
   "RandomId1": {
-    id: "user1", 
+    id: "RandomId1", 
     email: "user1@example.com", 
     password: "purple-monkey-dinosaur"
   },
  "RandomId2": {
-    id: "user2", 
+    id: "RandomId2", 
     email: "user2@example.com", 
     password: "dishwasher-funk"
+  },
+  "RandomId3": {
+    id: "RandomId2", 
+    email: "paulinate@o2.pl", 
+    password: "12"
   }
 };
 
@@ -34,7 +45,7 @@ const users = {
 //////Exercise examples/////////////////
 //displaying urlDatabase as json object on the website
 app.get("/", (request, response) => {
-  response.end("Hello World!");
+  response.end("Hello!This is a TinyApp!");
 });
 
 app.get("/urls.json", (request, response) => {
@@ -52,34 +63,42 @@ app.get("/hello", (request, response) => {
 ////////////GET METHODS/////////////////
 
 app.get("/urls", (request, response) => {
+  const userID = request.cookies["user_id"];
   let templateVars = { 
-    username: request.cookies["username"],
-    urls: urlDatabase 
+    user: users[userID],
+    urls: getUrlsOfOwner(userID) //urlDatabase 
   };
   response.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (request, response) => {
+  const userID = request.cookies["user_id"];
   let templateVars = { 
-    username: request.cookies["username"]
+    user: users[userID]
   };
+  if (userID === undefined){
+    response.redirect("/login")
+  } else {
   response.render("urls_new", templateVars);
+  }
 });
 
 app.get("/urls/:id", (request, response) => {
+  const userID = request.cookies["user_id"];
   let templateVars = { 
     shortURL: request.params.id, 
     urls: urlDatabase,
-    username: request.cookies["username"]
+    user: users[userID].email
   };
   response.render("urls_show", templateVars);
 });
 
 app.get("/urls/:id/update", (request, response) => {
+  const userID = request.cookies["user_id"];
   let templateVars = { 
     shortURL: request.params.id, 
-    urls: urlDatabase,
-    username: request.cookies["username"]
+    urls: urlDatabase,////users added from the 
+    user: users[userID].email
   };
   response.render("urls_show", templateVars);
 });
@@ -95,12 +114,21 @@ app.get("/u/:shortURL", (request, response) => {
 });
 
 app.get("/register", (request, response) => {
+  const userID = request.cookies["user_id"];
   let templateVars = { 
-    username: request.cookies["username"],
+    user: users[userID]
   };
   response.render("register", templateVars);
 });
 
+
+app.get("/login", (request, response) => {
+  const userID = request.cookies["user_id"];
+  let templateVars = { 
+    user: users[userID]
+  };
+  response.render("login", templateVars);
+});
 
 
 ////////////POST METHODS/////////////////
@@ -123,26 +151,19 @@ app.post("/urls/:id/update", (request, response) => {
   response.redirect("/urls");
 });
 
-app.post("/login", (request, response) => {
-  let username = request.body.username;
-  response.cookie( "username", username);
-  response.redirect("/urls");
-});
-
 //This is logout route which also clears the cookie
 app.post("/logout", (request, response) => {
-  response.clearCookie("username");
+  response.clearCookie("user_id");
   response.redirect("/urls");
 });
 
 app.post("/register", (request, response) => {
-
   let userID = generateRandomString();
   let email = request.body.email;
   let password = request.body.password;
   if (email && password){
     if ( doesEmailExists (email)){
-      response.status(400).send('Error 400. User already exists!');
+      response.status(400).send('<html><body><b><h1>Error 400!</h1><br><h2>User already exists!</h2></b></body></html>');
     } else {
       users[userID] = {
         id : userID,
@@ -154,28 +175,65 @@ app.post("/register", (request, response) => {
     response.redirect("/urls");
   }
   else {
-       response.status(400).send('Error 400! No email or/and password provides!');
-  };
-});
-  // console.log(users)
-function doesEmailExists(email){
-  for ( let user in users){
-    if( users[user].email === email){
-      return true;
-    }
+    response.status(400).send('Error 400! No email or/and password provides!');
   }
-};
-  
+});
+
+app.post("/login", (request, response) => {
+  let {email, password} = request.body;
+  const user = getUserByEmail(email);
+  if (user) {
+    if (user.password === password) {
+      response.cookie( "user_id", user.id);
+      return response.redirect("/");
+    }
+    return response.status(403).send('Error 403! Wrong password!');
+  }
+  else {
+    return response.status(403).send('Error 403! Wrong email!');
+  }
+});
 ///////////////////////////////////////////////
 app.listen(PORT, () => {
   console.log(`TinyApp listening on port ${PORT}!`);
 });
 
-function generateRandomString() {
+// Handling register 
+  function doesEmailExists(email){
+    for ( let user in users){
+      if( users[user].email === email){
+        return true;
+      }
+    }
+  };
+  
+  //Handling the Login
+  function getUserByEmail(email){
+    for ( let user in users){
+      if( users[user].email === email){
+        return users[user];
+      }
+    }
+    return null;
+  };
+//
+//
+  function getUrlsOfOwner(userID){
+    const userURLs = {};
+    for (let short in urlDatabase){
+      const urlObj = urlDatabase[short];
+      if (urlObj.userID === userID){
+        userURLs[userID] = urlDatabase[short];
+      }
+    }
+    return userURLs;
+  }
+  //Function generating a random string of 6 digits
+  function generateRandomString() {
   let randomStr = "";
   const items = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
   for (let i = 0; i < 6; i++)
     randomStr += items.charAt(Math.floor(Math.random() * items.length));
-  return randomStr;
+    return randomStr;
 }
